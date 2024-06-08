aa82be8b19735c491165b7059fc6ac191cb6fa3c
DennisNeu <76845672+DennisNeu@users.noreply.github.com>
2024-06-08 19:05:24 +0200

    Add playRound function
    Evaluates who wins the play

--- game.js
+++ game.js
@@ -1,7 +1,73 @@
 const choices = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
 
+function getHumanChoice() {
+    let choice;
+
+    while (isNaN(choice) || choice < 1 || choice > 3) {
+        choice = parseInt(prompt("Make a choice. 1 = rock, 2 = paper, 3 = scissors"));
+    }
+
+    return choice - 1;
+}
+
+function playRound(computerChoice, humanChoice) {
+    // 0 = rock
+    // 1 = paper
+    // 2 = scissors
+
+
+    if (humanChoice === computerChoice) {
+        console.log("Its a tie!");
+        return;
+    }
+
+    switch(humanChoice) {
+        case 0:
+            if (computerChoice === 1) {
+                console.log("You chose rock, computer chose paper. You lose");
+                computerScore++;
+                return;
+            }
+
+            if (computerChoice === 2) {
+                console.log("You chose rock, computer chose scissors. You win");
+                playerScore++;
+                return;
+            }
+        case 1:
+            if (computerChoice === 0) {
+                console.log("You chose paper, computer chose rock. You win");
+                playerScore++;
+                return;
+            }
+
+            if (computerChoice === 3) {
+                console.log("You chose paper, comupter chose scissors. You lose");
+                computerScore++;
+                return;
+            }
+
+        case 2:
+            if (computerChoice === 0) {
+                console.log("You chose scissors, computer chose rock. You lose");
+                computerScore++;
+                return;
+            }
+
+            if (computerChoice === 1) {
+                console.log("You chose scissors, comupter chose paper. You win");
+                playerScore++;
+                return;
+            }
+        
+        
+    }
+    
+}
